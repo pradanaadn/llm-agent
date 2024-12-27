948a1828ec3b23240f2867bbc1977ed877c65104
pradanaadn <pradanaadn21@gmail.com>
2024-12-27 21:21:15 +0800

    chore: add cv metadata extractor

---
+++ core/data/extractor.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+def extract_metadata(df):
+    metadata = {}
+
+    # Number of columns
+    metadata['Number of Columns'] = df.shape[1]
+
+    # Column names
+    metadata['Schema'] = df.columns.tolist()
+
+    # Data types of each column
+    metadata['Data Types'] = str(df.dtypes)
+
+    # Summary statistics
+    metadata['Sample'] = df.head(1).to_dict(orient="records")
+
+    return metadata
